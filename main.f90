9237d50210f913405c54f0f0d6e18263f22c11a9
ZY Yang <zyyang@ZYs-Mac-mini.home>
2024-05-31 14:37:58 -0600

    refine add command

--- main.f90
+++ main.f90
@@ -8,14 +8,14 @@ implicit none
 
     do
         if (radius <= 0) then 
-            ! print *, error
+            ! print error message
             print *, "Invalid radius value! Please enter a positive value:"
-            read *, radius
+            read (*,*) radius
         else
             call sphere_volume_calculator(radius, volume)
             print *, "The volume of this sphere is", volume
             print *, "Enter another radius:"
-            read *, radius
+            read (*,*) radius
         end if 
     end do
 end program sphere_volume
